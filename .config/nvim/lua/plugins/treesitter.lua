74175fa39569d33ff268001fc2e0a991261f51fc
lik <117955767+l1qued@users.noreply.github.com>
2025-11-05 09:24:08 +0900

    Delete .config/nvim directory

--- .config/nvim/lua/plugins/treesitter.lua
+++
@@ -1,6 +0,0 @@
-require("nvim-treesitter.configs").setup({
-	ensure_installed = { "lua", "typescript", "python", "javascript", "html", "css", "html", "php", "blade" },
-	sync_install = false,
-	highlight = { enable = true },
-	indent = { enable = true },
-})
