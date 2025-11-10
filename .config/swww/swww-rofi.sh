0233debeac673cc84f45720c323ba20a59f03528
lik <117955767+l1qued@users.noreply.github.com>
2025-11-10 21:15:18 +0900

    Create swww-rofi.sh

---
+++ .config/swww/swww-rofi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+wallpaper_dir="$HOME/Wallpapers"
+
+find "$wallpaper_dir" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" -o -iname "*.gif" \) -print0 | \
+while IFS= read -r -d $'\0' file; do
+    printf "%s\0icon\x1f%s\n" "$(basename "$file")" "$file"
+done | \
+rofi -dmenu -i -p "wallpaper" -markup-rows -width 40 -lines 15 -theme ~/.config/rofi/wl.rasi | \
+while IFS= read -r selected_wallpaper; do
+  if [[ -n "$selected_wallpaper" ]]; then
+    swww img "$wallpaper_dir/$selected_wallpaper" --transition-type=wipe --transition-angle=30 --transition-fps=180
+  fi
+done
